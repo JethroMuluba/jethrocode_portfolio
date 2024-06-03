6e55c7ea78c712177a196e71217b88d448049806
JethroMuluba <jethro.muluba@gmail.com>
2024-06-03 14:49:30 +0100

    Feat : Create Hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './components/Header'
+import Hero from './components/Hero'
 
 function App() {
 
   return (
 <main>
     <Header/>
+    <Hero/>
 </main>
   )
 }
