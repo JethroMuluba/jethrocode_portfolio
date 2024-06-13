8c6ef3e4a2fa8ca598fc39f6d9bfa13902583571
JethroMuluba <jethro.muluba@gmail.com>
2024-06-13 11:09:11 +0100

    Feat : Create the title of work experiences

--- src/components/WorkExperiences.jsx
+++ src/components/WorkExperiences.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
-
+import React from 'react';
+import data from '../data/data.json';
 function WorkExperiences() {
+
+    const underTitle = data.workExperiences[0].underTitle;
   return (
-    <div id="experiences">WorkExperiences</div>
+    <section id="experiences" className=' w-full flex flex-col gap-8 items-center text-center py-[50px]'>
+        <div className=' flex flex-col gap-2 items-center md:w2/4   '>
+            <h3 className=' font-semibold text-white text-[32px] md:text-[42px] md:text-center' >Work Experiences</h3>
+            <span className=' w-[96px] border-b-2 border-[#FEB633]'></span>
+        </div>
+
+        <p className='text-[#8D9092]  px-6 md:px-[300px]  md:p-0 '> {underTitle} </p>
+
+
+
+
+    </section>
   )
 }
 
