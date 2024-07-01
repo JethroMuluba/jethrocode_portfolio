50e627877506b31a62171ef463155d5f4165d024
JethroMuluba <jethro.muluba@gmail.com>
2024-07-01 20:18:16 +0100

    Style : Make Form Responsive For Desktop

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -17,7 +17,7 @@ function ContactForm() {
         {errors.name && <span style={{color: '#DA5643', fontSize: '14px'}}>Name must have at least 3 letters</span>}
       </div>
 
-      <div className=' flex flex-col gap-[28px]'>
+      <div className=' flex flex-col gap-[28px] md:flex-row'>
         <div className=' flex flex-col items-start  gap-2'>
           <label htmlFor="phone" className=' text-base text-[#8D9092]'>PHONE NUMBER</label>
           <input {...register("phone", {required:true})} type="number" className=' bg-[#303131] h-[55px] rounded-[5px] px-5 w-[100%] text-[#fff] placeholder:text-[#8D9092] placeholder:opacity-20 outline-none appearance-none caret-[#fff] ' placeholder='Tape your phone number' />
@@ -29,15 +29,15 @@ function ContactForm() {
           <input {...register("email", {required:true, minLength: 5, maxLength:60})} type="email" className=' bg-[#303131] h-[55px] rounded-[5px] px-5 w-[100%] text-[#fff] placeholder:text-[#8D9092] placeholder:opacity-20 outline-none appearance-none caret-[#fff] ' placeholder='Tape your name' />
           {errors.email && <span style={{color: '#DA5643', fontSize: '14px'}}>Email must have at least 5 letters</span>}
         </div>
+      </div>
 
-        <div className=' flex flex-col items-start  gap-2'>
+      <div className=' flex flex-col items-start  gap-2'>
           <label htmlFor="message" className=' text-base text-[#8D9092]'>YOUR MESSAGE</label>
           <textarea {...register("message", {required:true, minLength: 10, maxLength:500})} name="message" id="" className=' bg-[#303131] h-[186px] rounded-[5px] p-5 w-[100%] text-[#fff] placeholder:text-[#8D9092] placeholder:opacity-20 outline-none appearance-none caret-[#fff] ' placeholder='Tape your message here'></textarea>
           {errors.message && <span style={{color: '#DA5643', fontSize: '14px'}}>You must message me to get more informations</span>}  
-        </div>
-
-        <button type='submit' className=' bg-[#FEB633] hover:bg-[#303131] text-sm font-semibold hover:border hover:border-[#FEB633] text-white hover:text-[#FEB633] py-[14px] px-[50px] md:ml-0   rounded-md active:border-2 active:border-[#fff]'>Send Message </button>
       </div>
+
+      <button type='submit' className=' bg-[#FEB633] md:w-[213px] hover:bg-[#303131] text-sm font-semibold hover:border hover:border-[#FEB633] text-white hover:text-[#FEB633] py-[14px] px-[50px] md:ml-0   rounded-md active:border-2 active:border-[#fff]'>Send Message </button>
 
     </form>
   )
