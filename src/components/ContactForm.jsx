b790f89967a9c848fcdb077aafad54a183f652a9
JethroMuluba <jethro.muluba@gmail.com>
2024-07-01 19:43:53 +0100

    Style : Fix the Size Of Require Message

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -6,27 +6,28 @@ function ContactForm() {
         const { register, handleSubmit, formState : {errors}, reset} = useForm();
 
           const onSubmitForm = (data) => {
-            alert(`Client name is ${data.name}, phone number is ${data.phone}, email is ${data.email} and message is ${data.message}`)
+            alert(`Client name is ${data.name}, phone number is ${data.phone}, email is ${data.email} and message is ${data.message}`);
+            reset();
           }
   return (
     <form onSubmit={handleSubmit(onSubmitForm)} className=' flex flex-col gap-[28px]'>
       <div className=' flex flex-col items-start  gap-2'>
         <label htmlFor="name" className=' text-base text-[#8D9092]'>NAME</label>
         <input {...register("name", {required:true, minLength: 3, maxLength:60})} type="text" className=' bg-[#303131] h-[55px] rounded-[5px] px-5 w-[100%] text-[#fff] placeholder:text-[#8D9092] placeholder:opacity-20 outline-none appearance-none caret-[#fff] ' placeholder='Tape your name' />
-        {errors.name && <span style={{color: '#DA5643', fontSize: '12px'}}>Name must have at least 3 letters</span>}
+        {errors.name && <span style={{color: '#DA5643', fontSize: '14px'}}>Name must have at least 3 letters</span>}
       </div>
 
       <div className=' flex flex-col gap-[28px]'>
         <div className=' flex flex-col items-start  gap-2'>
           <label htmlFor="phone" className=' text-base text-[#8D9092]'>PHONE NUMBER</label>
           <input {...register("phone", {required:true})} type="number" className=' bg-[#303131] h-[55px] rounded-[5px] px-5 w-[100%] text-[#fff] placeholder:text-[#8D9092] placeholder:opacity-20 outline-none appearance-none caret-[#fff] ' placeholder='Tape your phone number' />
-          {errors.phone && <span style={{color: '#DA5643', fontSize: '12px'}}>Phone is required</span>}
+          {errors.phone && <span style={{color: '#DA5643', fontSize: '14px'}}>Phone is required</span>}
         </div>
 
         <div className=' flex flex-col items-start  gap-2'>
           <label htmlFor="email" className=' text-base text-[#8D9092]'>Email</label>
           <input {...register("email", {required:true, minLength: 5, maxLength:60})} type="email" className=' bg-[#303131] h-[55px] rounded-[5px] px-5 w-[100%] text-[#fff] placeholder:text-[#8D9092] placeholder:opacity-20 outline-none appearance-none caret-[#fff] ' placeholder='Tape your name' />
-          {errors.email && <span style={{color: '#DA5643', fontSize: '12px'}}>Email must have at least 5 letters</span>}
+          {errors.email && <span style={{color: '#DA5643', fontSize: '14px'}}>Email must have at least 5 letters</span>}
         </div>
 
         <div className=' flex flex-col items-start  gap-2'>
