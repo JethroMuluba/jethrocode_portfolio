3f10f50a346646f8068dd73d3a310351e71b1fde
JethroMuluba <jethro.muluba@gmail.com>
2024-07-12 13:42:04 +0100

    Style : Add Motion for Body Section

--- src/App.jsx
+++ src/App.jsx
@@ -9,23 +9,30 @@ import WorkExperiences from './components/WorkExperiences'
 import ContactMe from './components/ContactMe'
 import ScrollToTopButton from './components/ScrollTopButton'
 import Footer from './components/Footer'
+import { AnimatePresence, motion } from 'framer-motion'
 
 function App() {
 
   return (
-<body>
-    <Header/>
-    <main className=' absolute top-[55px]'>
-      <Hero/>
-      <AboutMe/>
-      {/* <Skills/> */}
-      <Realisations/>
-      <WorkExperiences/>
-      <ContactMe/>
-      <Footer/>
-      <ScrollToTopButton/>
-    </main>
-</body>
+    <AnimatePresence>
+    <motion.body
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.5 }}>
+        <Header/>
+        <main className=' absolute top-[55px]'>
+          <Hero/>
+          <AboutMe/>
+          {/* <Skills/> */}
+          <Realisations/>
+          <WorkExperiences/>
+          <ContactMe/>
+          <Footer/>
+          <ScrollToTopButton/>
+        </main>
+    </motion.body>
+    </AnimatePresence>
   )
 }
 
