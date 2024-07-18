713c9822b4ead85488c07232bdbcbdb72f16e56f
JethroMuluba <jethro.muluba@gmail.com>
2024-07-18 21:04:18 +0100

    Feat : Create View Work Table

--- src/components/ContactMe.jsx
+++ src/components/ContactMe.jsx
@@ -5,7 +5,7 @@ import ContactForm from './ContactForm';
 
 function ContactMe() {
   return (
-    <div id="contact-me" className=' w-full flex flex-col gap-8 items-center  text-center px-6 py-[50px]'>
+    <section id="contact-me" className=' w-full flex flex-col gap-8 items-center  text-center px-6 py-[50px]'>
           <div className=' flex flex-col gap-2  items-center md:w2/4 '>
             <h3 className=' font-semibold text-white text-[32px] md:text-[42px] md:text-center' >Contact Me</h3>
             <span className=' w-[96px] border-b-2 border-[#FEB633]'></span>
@@ -21,7 +21,7 @@ function ContactMe() {
           
         </div>
 
-    </div>
+    </section>
   )
 }
 
